c5c04ae18c8667864079164328f9a4d3d3050150
realizer12 <dlwldlwldus123@gmail.com>
2024-12-02 00:35:15 +0900

    👔 즐겨찾기 화면 처리 추가

--- presentation/src/main/java/com/happymoonday/presentation/feature/favorite/fragment/FavoriteFragment.kt
+++ presentation/src/main/java/com/happymoonday/presentation/feature/favorite/fragment/FavoriteFragment.kt
@@ -1,8 +1,18 @@
 package com.happymoonday.presentation.feature.favorite.fragment
 
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.viewModels
 import com.happymoonday.presentation.R
 import com.happymoonday.presentation.base.BaseFragment
 import com.happymoonday.presentation.databinding.FragmentFavoriteBinding
+import com.happymoonday.presentation.feature.detail.activity.ProductDetailActivity
+import com.happymoonday.presentation.feature.favorite.viewmodel.FavoriteViewModel
+import com.happymoonday.presentation.feature.search.activity.SearchActivity.Companion.CLICKED_ART_PRODUCT_INFO
+import com.happymoonday.presentation.feature.search.adapter.SearchItemRvAdapter
+import com.happymoonday.presentation.model.SemaPsgudInfoKorInfoRowUiModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Create Date: 2024. 11. 24.
@@ -12,7 +22,15 @@ import com.happymoonday.presentation.databinding.FragmentFavoriteBinding
  *
  *
  **/
+@AndroidEntryPoint
 class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(R.layout.fragment_favorite) {
+
+    private val favoriteViewModel: FavoriteViewModel by viewModels()
+
+    private lateinit var favoriteAdapter: SearchItemRvAdapter
+    private lateinit var startActivityResultLauncher: ActivityResultLauncher<Intent>
+
+
     override fun FragmentFavoriteBinding.onCreateView() {
         initSet()
         setListenerEvent()
@@ -20,14 +38,35 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(R.layout.fragment
     }
 
     private fun initSet() {
-
+        favoriteAdapter = SearchItemRvAdapter()
+        binding.rvFavorite.apply {
+            adapter = favoriteAdapter
+        }
     }
 
     private fun setListenerEvent() {
+        //activity start result 받아옴.
+        startActivityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                //즐겨찾기 완료이므로 즐겨찾기 탭으로 이동
+                if(result.resultCode == ProductDetailActivity.BOOK_MARK_FINISH_RESULT_CODE){
+                    favoriteViewModel.getBookMarkList()
+                }
+            }
 
+        favoriteAdapter.setItemClickListener(object : SearchItemRvAdapter.ItemClickListener {
+            override fun onClickItem(item: SemaPsgudInfoKorInfoRowUiModel) {
+                startActivityResultLauncher.launch(Intent(activity?:return, ProductDetailActivity::class.java).apply {
+                    putExtra(CLICKED_ART_PRODUCT_INFO, item)
+                })
+            }
+        })
     }
 
     private fun getDataFromVm() {
-
+        favoriteViewModel.bookMarkList.observe(viewLifecycleOwner) {
+            favoriteAdapter.submitList(null)
+            favoriteAdapter.submitList(it)
+        }
     }
 }
---
+++ presentation/src/main/java/com/happymoonday/presentation/feature/favorite/viewmodel/FavoriteViewModel.kt
@@ -0,0 +1,45 @@
+package com.happymoonday.presentation.feature.favorite.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.happymoonday.domain.usecase.GetBookMarkListUseCase
+import com.happymoonday.presentation.model.SemaPsgudInfoKorInfoRowUiModel
+import com.happymoonday.presentation.model.SemaPsgudInfoKorInfoRowUiModel.Companion.fromEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * Create Date: 2024. 12. 2.
+ *
+ * 즐겨찾기 화면 ViewModel
+ * @author LeeDongHun
+ *
+ *
+**/
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(
+    private val getBookMarkListUseCase: GetBookMarkListUseCase,
+):ViewModel() {
+
+    //북마크 리스트 뷰로 보냄.
+    private val _bookMarkList = MutableLiveData<List<SemaPsgudInfoKorInfoRowUiModel>>()
+    val bookMarkList: LiveData<List<SemaPsgudInfoKorInfoRowUiModel>> = _bookMarkList
+
+    init {
+        getBookMarkList()
+    }
+
+    // 북마크 리스트 가져와서 현재
+    //상세 화면 상품과 같은 것이 있는지 체크
+    fun getBookMarkList() = viewModelScope.launch {
+        getBookMarkListUseCase()
+            .onSuccess {
+                _bookMarkList.value = it.map { it.fromEntity() }
+            }
+    }
+
+
+}
