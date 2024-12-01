c4f864ec8bb0f9bb9e695b6e43606e80b7b5398b
realizer12 <dlwldlwldus123@gmail.com>
2024-12-02 01:58:38 +0900

    🐛 탭전환 빠르게 할시 죽는 현상 수정

--- presentation/src/main/java/com/happymoonday/presentation/feature/favorite/fragment/FavoriteFragment.kt
+++ presentation/src/main/java/com/happymoonday/presentation/feature/favorite/fragment/FavoriteFragment.kt
@@ -1,8 +1,6 @@
 package com.happymoonday.presentation.feature.favorite.fragment
 
 import android.content.Intent
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import com.happymoonday.presentation.R
 import com.happymoonday.presentation.base.BaseFragment
@@ -26,15 +24,17 @@ import dagger.hilt.android.AndroidEntryPoint
 class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(R.layout.fragment_favorite) {
 
     private val favoriteViewModel: FavoriteViewModel by viewModels()
-
     private lateinit var favoriteAdapter: SearchItemRvAdapter
-    private lateinit var startActivityResultLauncher: ActivityResultLauncher<Intent>
-
 
     override fun FragmentFavoriteBinding.onCreateView() {
         initSet()
         setListenerEvent()
         getDataFromVm()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        favoriteViewModel.getBookMarkList()
     }
 
     private fun initSet() {
@@ -45,27 +45,17 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(R.layout.fragment
     }
 
     private fun setListenerEvent() {
-        //activity start result 받아옴.
-        startActivityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                //즐겨찾기 완료이므로 즐겨찾기 탭으로 이동
-                if(result.resultCode == ProductDetailActivity.BOOK_MARK_FINISH_RESULT_CODE){
-                    favoriteViewModel.getBookMarkList()
-                }
-            }
-
         favoriteAdapter.setItemClickListener(object : SearchItemRvAdapter.ItemClickListener {
             override fun onClickItem(item: SemaPsgudInfoKorInfoRowUiModel) {
-                startActivityResultLauncher.launch(Intent(activity?:return, ProductDetailActivity::class.java).apply {
+                startActivity((Intent(activity ?: return, ProductDetailActivity::class.java).apply {
                     putExtra(CLICKED_ART_PRODUCT_INFO, item)
-                })
+                }))
             }
         })
     }
 
     private fun getDataFromVm() {
         favoriteViewModel.bookMarkList.observe(viewLifecycleOwner) {
-            favoriteAdapter.submitList(null)
             favoriteAdapter.submitList(it)
         }
     }
